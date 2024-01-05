734b2c30579bbc20f1dd91454ff6809c35ab6f9c
Sumon Basak <127886578+sumbasak@users.noreply.github.com>
2024-01-05 19:12:09 +0530

    Update lj_code.py

--- lennard-jones-code/lj_code.py
+++ lennard-jones-code/lj_code.py
@@ -1,6 +1,3 @@
-import random as rd
-import numpy as np
-
 def initiate_loc_vel_acc(ndim, npart, boxsize):
     '''
     Providing locations and velocities in all three axes from random
@@ -226,43 +223,7 @@ def update_vel_unscaled(vel, dt, acc):
 import numpy as np
 import random as rd
 
-# parameters
 
-ndim = 3
-nscans = 1000
-npart = 100
-dt = 1.0e-4
-reference_temp = 300.0 
-sig = 1.0
-eps = 1.0
-r_cutoff = 2.5 * sig 
-
-# calculate LJ potential
-u_at_cutoff = 4.0 * eps * ((sig / r_cutoff) ** 12 - (sig / r_cutoff) ** 6)
-
-# calculate the derivative of potential energy at cutoff
-term1 = (sig ** 12) / (r_cutoff ** 7)
-term2 = (sig ** 6) / (r_cutoff ** 7)
-du_at_cutoff = 48.0 * eps * (term1 - 0.5 * term2)
-
-boxsize = 10.0 * sig
-volume = boxsize ** 3
-rho = npart / volume
-
-[total_pressure, total_kin_en, total_pot_en, total_temp] = np.zeros([4])
-
-# Create the files to write the data
-f_tpzsc = open("tpzsc.out", "w")
-f_kpesc = open("kpesc.out", "w")
-f_xyzsc = open("loc_sc.xyz", "w")
-
-ignore_steps = 30
-
-check_temp20 = np.zeros(nscans)
-check_pressure20 = np.zeros(nscans)
-check_kin_en20 = np.zeros(nscans)
-check_pot_en20 = np.zeros(nscans)
-check_total_en20 = np.zeros(nscans)
 #............................................................................
 #............................................................................
 #............................................................................
