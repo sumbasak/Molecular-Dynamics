15d96ae31e66eb7a4a95a9bea5b5381c47fb6528
Sumon Basak <127886578+sumbasak@users.noreply.github.com>
2024-01-19 10:45:33 +0530

    Update protein-simulation-all-steps.sh

--- bash-automation/protein-simulation-all-steps.sh
+++ bash-automation/protein-simulation-all-steps.sh
@@ -1,71 +1,78 @@
 #!/bin/bash
 
-cd /home/sumon/MEGA/tutorials/pradiptabash/
+# This script goes to a specific folder and performs a set of simulations for 1 ns. 
+# Starting from protein structure download from the data bank, it goes on to simulate NVT,
+# NPT, and then, finally the trajectory. All the RMSD values calculated for a range of 
+# temperatures are placed in one file. 
 
-#download the pdb file from the RCSB server
+file_path=/home/sumon/MEGA/tutorials/pradiptabash/
+
+cd "$file_path"
+
+# Download the pdb file from the RCSB server
 wget https://files.rcsb.org/download/1VII.pdb
 
-#clean the water from the pdb file
+# Clean the water from the pdb file
 grep -v HOH 1VII.pdb > 1VII_clean.pdb
 
-#process the file from pdb to gmx
+# Process the file from pdb to gmx
 echo -e "15" | gmx pdb2gmx -f 1VII_clean.pdb -o 1VII_processed.gro -water spce -ignh
 
-#get the protein inside a box
+# Get the protein inside a box
 gmx editconf -f 1VII_processed.gro -o 1VII_newbox.gro -c -d 1.0 -bt cubic
 
-#get the protein solvated with water
+# Get the protein solvated with water
 gmx solvate -cp 1VII_newbox.gro -cs spc216.gro -o 1VII_solv.gro -p topol.top
 
-#copy the ion.mdp file from my other directory
+# Copy the ion.mdp file from my other directory
 cp /home/sumon/MEGA/tutorials/lysozymeInWater/*.mdp .
 
-#get ions solvated in the water-protein system
+# Get ions solvated in the water-protein system
 gmx grompp -f ions.mdp -c 1VII_solv.gro -p topol.top -o ions.tpr
 echo -e "13" | gmx genion -s ions.tpr -o 1VII_solv_ions.gro -p topol.top -pname NA -nname CL -neutral 
 
-#minimize the energy of the system
+# Minimize the energy of the system
 gmx grompp -f minim.mdp -c 1VII_solv_ions.gro -p topol.top -o em.tpr 
 gmx mdrun -v -deffnm em 
 
-#nvt simulation
+# NVT simulation
 gmx grompp -f nvt.mdp -c em.gro -r em.gro -p topol.top -o nvt.tpr
 gmx mdrun -deffnm nvt
 
-#npt simulation
+# NPT simulation
 gmx grompp -f npt.mdp -c nvt.gro -r nvt.gro -t nvt.cpt -p topol.top -o npt.tpr
 gmx mdrun -deffnm npt
 
-#change the temperature inside to a known phrase
+# Change the temperature inside to a known phrase
 sed -i 's/300/sbk/g' md.mdp
 
-#run the loop to copy all the files and run individual simulations
+# Run the loop to copy all the files and run individual simulations
 for temp in {300..320..5}; do 
 
-    #make a new directory
+    # Make a new directory
     mkdir "$temp"
 
-    #copy the requisite files to that new directory
+    # Copy the requisite files to that new directory
     cp md.mdp npt.cpt npt.gro topol.top $temp/  
 
-    #navigate to that directory
+    # Navigate to that directory
     cd "$temp"/
 
-    #change sbk with the temperature
+    # Change sbk with the temperature
     sed -i 's/sbk/'"$temp"'/g' md.mdp
 
-    #final mdrun for that specific temperature
+    # Final mdrun for that specific temperature
     gmx grompp -f md.mdp -c npt.gro -t npt.cpt -p topol.top -o md_0_1.tpr 
     gmx mdrun -deffnm md_0_1
 
-    #centering the protein inside the box
+    # Centering the protein inside the box
     echo -e "1 0" | gmx trjconv -s md_0_1.tpr -f md_0_1.xtc -o md_0_1_noPBC.xtc -pbc mol -center 
     
-    #calculation of rmsd values for all the temperatures
+    # Calculation of rmsd values for all the temperatures
     echo -e "4
     1" | gmx rms -s md_0_1.tpr -f md_0_1_noPBC.xtc -o rmsd.xvg -tu ns 
 
-    cd ../
+    cd "$file_path"
 done
 
 # Output file
