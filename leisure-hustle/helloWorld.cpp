52e8351f3c02befc92201727783717826d616505
Sumon Basak <127886578+sumbasak@users.noreply.github.com>
2024-01-19 14:05:41 +0530

    Update helloWorld.cpp

--- leisure-hustle/helloWorld.cpp
+++ leisure-hustle/helloWorld.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
-using namespace std;
+using namespace std; // cout is part of it
+
+/* Just like Python triple quote, C++ allows docstring writing
+with this particular syntax of '/star...star/'. In between, anything that
+is written won't be considered an executable line. */
 
 int main() {
   cout << "Hello World!";
